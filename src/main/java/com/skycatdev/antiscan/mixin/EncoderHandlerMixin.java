d476f5aa38954086a1496198926f6dfd96fa8b9a
skycatminepokie <52087591+skycatminepokie@users.noreply.github.com>
2025-04-30 23:11:48 -0600

    Darn intellij reformatting my darn stuff out of the darn code

--- src/main/java/com/skycatdev/antiscan/mixin/EncoderHandlerMixin.java
+++ src/main/java/com/skycatdev/antiscan/mixin/EncoderHandlerMixin.java
@@ -5,7 +5,12 @@ import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
 import com.skycatdev.antiscan.AntiScan;
 import io.netty.channel.ChannelHandlerContext;
+//? if >=1.20.5
 import net.minecraft.network.handler.EncoderHandler;
+//? if <1.20.5 && >1.20.1
+/*import net.minecraft.network.handler.PacketEncoder;*/
+//? if <=1.20.1
+/*import net.minecraft.network.PacketEncoder;*/
 import org.slf4j.Logger;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
